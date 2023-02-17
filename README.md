e5052ccf153ff9f708653c682df83b3fabf305c5
AlliysonBonner <alliyson.bonner@gmail.com>
2023-02-17 00:21:11 -0600

    Optimized code, added image and license  This commit optimizes the rock-paper-scissors game code to be more efficient and added google-style docstrings and comments. The readme.md file has been updated to include a brief description of the project, instructions for how to play the game, and an image. The license.md file has also been added to the repository.   Changes Made: - Optimized code to make it more efficient - Added google-style docstrings and comments to the code - Updated the readme.md file to include a brief description of the project, instructions for how to play the game, and an image - Added a license.md file to the repository

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# RockPaperScissors
+# Rock Paper Scissors Game
+![A cartoon of two young men locked in an aggressive match of Rock, Paper, Scissors.](https://github.com/AlliysonBonner/RockPaperScissors/blob/main/image.png?raw=true)
+This is a simple implementation of the popular hand game Rock Paper Scissors. The game is played between two players, where each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock" (a closed fist), "paper" (a flat hand), and "scissors" (a fist with the index finger and middle finger extended, forming a V). "Scissors" is identical to the two-fingered V sign (also indicating "victory" or "peace") except that it is pointed horizontally instead of being held upright in the air.
+
+## Getting Started
+To run the game, simply run the play() function in the terminal.
+
+## Rules
+In this game, rock beats scissors, scissors beat paper, and paper beats rock.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/alliysonbonner/RockPaperScissors/LICENSE.md) file for details.
+
+## Author
+* Name: Alliyson Bonner
+* Github: https://github.com/alliysonbonner
+* LinkedIn: https://www.linkedin.com/in/alliyson
+* Email: alliyson.bonner@gmail.com
+
+## Acknowledgments
+This game was developed as part of a software engineering training program. Special thanks to our instructors and mentors for their guidance and support throughout the project.
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,69 +1,39 @@
-# Rock Paper Scissors
-# a hand game originating from China, usually played between two people,
-# in which each player simultaneously forms one of three shapes with an
-# outstretched hand. These shapes are "rock" (a closed fist), "paper" 
-# (a flat hand), and "scissors" (a fist with the index finger and middle 
-# finger extended, forming a V). "Scissors" is identical to the 
-# two-fingered V sign (also indicating "victory" or "peace") except 
-# that it is pointed horizontally instead of being held upright in the 
-# air.
-
-# Author: Alliyson Bonner
-# Github: https://github.com/alliysonbonner
-# Linkedin: https://www.linkedin.com/in/alliyson
-# Email: alliyson.bonner@gmail.com
-# Date: December 07 2022
-
-
-# Import Library: random
 import random
 
-def is_win(player, opponent):
-  """is_win determines if the player beats the opponent or not.
+def is_win(player: str, opponent: str) -> bool:
+    """Determines if the player beats the opponent or not.
 
-     In this game rock beat scissors, scissors beat paper, and
-     paper beats rock.
-  
-  Args:
-      player(str): This is the choice that the human player 
-      made in the game. For rock the string is 'r', for scissor
-      the string is 's' and for paper the string is 'p'.
-      
-      opponent(str): This is the choice that the computer made 
-      during the game. For rock the string is 'r', for scissor
-      the string is 's' and for paper the string is 'p'.
-      
-  Returns(bool):
-      True if the player beats the opponent. False if the
-      opponent beats the player.
-  """
-  game_state = (player, opponent)
-  return game_state == ('r', 's') or game_state == ('s', 'p') or game_state == ('p', 'r')
-  
-  
-def play():
-  """play starts the game of rock paper scissors
+    Args:
+        player (str): The choice the human player made in the game.
+            'r' for rock, 's' for scissors, and 'p' for paper.
+        opponent (str): The choice the computer made in the game.
+            'r' for rock, 's' for scissors, and 'p' for paper.
 
-  Returns(str):
-      tells whether or not you won the game.
-  """
-  # Select r, p, or s for the user and computer section in the game 
-  user_choice = input("'r' for rock, 'p' for paper, 's' for scissors:\n")
-  computer_choice = random.choice(['r', 'p', 's'])
-	
-  # returns the game results depending on if it is a tie, win, or loss.
-  if user_choice == computer_choice:
-    return 'It\'s a tie'
-  
-  elif is_win(user_choice, computer_choice):
-    return 'You Won!'
-  
-  return 'HaHaHaHa, You Lost!'
+    Returns:
+        bool: True if the player beats the opponent. False if the
+            opponent beats the player.
+    """
+    return (player == 'r' and opponent == 's') or \
+           (player == 's' and opponent == 'p') or \
+           (player == 'p' and opponent == 'r')
 
-if __name__ == "__main__":
-  # Welcome players to the game
-  print("Welcome to Rock! Paper! Scissors!")
-  # Call funtion to start game
-  game_results = play()
-  # Display results: tied, won, or lost
-  print(game_results)
+def play() -> str:
+    """Starts the game of rock, paper, scissors.
+
+    Returns:
+        str: The game result, whether it's a tie, win, or loss.
+    """
+    user_choice = input("Type 'r' for rock, 'p' for paper, 's' for scissors: ").lower()
+    computer_choice = random.choice(['r', 'p', 's'])
+
+    if user_choice == computer_choice:
+        return 'It\'s a tie!'
+    elif is_win(user_choice, computer_choice):
+        return 'You won!'
+    else:
+        return 'You lost!'
+
+if __name__ == '__main__':
+    print('Welcome to Rock, Paper, Scissors!')
+    game_result = play()
+    print(game_result)
